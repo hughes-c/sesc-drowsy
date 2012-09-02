7942c4b8a8138983f742d6091730769feb6d2b98
Hughes <hughes@fraidy-nix.(none)>
2012-09-02 10:29:56 -0500

    Removed build directory

--- build/config.h
+++
@@ -1,1 +0,0 @@
-#define COMPOPTS " -DPACKAGE_NAME=\"esesc\" -DPACKAGE_TARNAME=\"esesc\" -DPACKAGE_VERSION=\"2\" -DPACKAGE_STRING=\"esesc\ 2\" -DPACKAGE_BUGREPORT=\"renau@soe.ucsc.edu\ luisceze@cs.uiuc.edu\""
